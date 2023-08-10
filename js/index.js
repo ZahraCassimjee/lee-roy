b3fb426c4c1e5f60f2f84faa75063eaf044507b7
ZahraCassimjee <231131@virtualwindow.co.za>
2023-08-10 21:37:10 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,93 +1,92 @@
 const trips = [
     {
-        
-        name: "Caribbean Cruise",
-        destination: "Caribbean Islands",
-        price: "12000",
+        name: "Tropical Paradise Escape",
+        destination: "Fiji Islands",
+        price: "18000",
         dateOfDeparture: "2023-09-15",
         duration: "7 days",
-        tripCode: "CAR123",
+        tripCode: "TPL123",
+        image: "card 1.jpg"
+    },
+    {
+        name: "Exotic Hawaiian Getaway",
+        destination: "Hawaii, USA",
+        price: "25000",
+        dateOfDeparture: "2023-08-20",
+        duration: "10 days",
+        tripCode: "HAW456",
         image: "card 2.jpg"
     },
     {
-        name: "Mediterranean Adventure",
-        destination: "Mediterranean Sea",
-        price: "15000",
-        dateOfDeparture: "2023-08-20",
-        duration: "10 days",
-        tripCode: "MED456",
-        image: "card 8.jpg"
-    },
-    {
-        name: "Alaskan Expedition",
-        destination: "Alaska, USA",
-        price: "18000",
+        name: "Caribbean Island Adventure",
+        destination: "Caribbean Islands",
+        price: "22000",
         dateOfDeparture: "2023-07-10",
         duration: "14 days",
-        tripCode: "ALA789",
+        tripCode: "CAR789",
         image: "card 3.jpg"
     },
     {
-        name: "Tropical Paradise",
-        destination: "Hawaii, USA",
-        price: "14000",
+        name: "Mauritius Dream Vacation",
+        destination: "Mauritius",
+        price: "21000",
         dateOfDeparture: "2023-08-05",
         duration: "8 days",
-        tripCode: "TRO101",
-        image: "card 6.jpeg"
-    },
-    {
-        name: "European Escapade",
-        destination: "Various European Cities",
-        price: "25000",
-        dateOfDeparture: "2023-09-01",
-        duration: "12 days",
-        tripCode: "EUR202",
-        image: "card 5.jpg"
-    },
-    {
-        name: "Exotic Asia Tour",
-        destination: "Southeast Asia",
-        price: "22000",
-        dateOfDeparture: "2023-10-10",
-        duration: "15 days",
-        tripCode: "ASI303",
+        tripCode: "MAU101",
         image: "card 4.jpg"
     },
     {
-        name: "Australian Adventure",
-        destination: "Australia",
-        price: "19000",
-        dateOfDeparture: "2023-11-20",
-        duration: "11 days",
-        tripCode: "AUS404",
-        image: "card 9.jpg"
+        name: "Bali Bliss Retreat",
+        destination: "Bali, Indonesia",
+        price: "27000",
+        dateOfDeparture: "2023-09-01",
+        duration: "12 days",
+        tripCode: "BAL202",
+        image: "card 5.jpg"
     },
     {
-        name: "South American Discovery",
-        destination: "South America",
-        price: "23000",
-        dateOfDeparture: "2023-12-05",
-        duration: "13 days",
-        tripCode: "SAM505",
+        name: "Tahiti Island Paradise",
+        destination: "Tahiti, French Polynesia",
+        price: "29000",
+        dateOfDeparture: "2023-10-10",
+        duration: "15 days",
+        tripCode: "TAH303",
+        image: "card 6.jpeg"
+    },
+    {
+        name: "Maldives Serenity Cruise",
+        destination: "Maldives",
+        price: "32000",
+        dateOfDeparture: "2023-11-20",
+        duration: "11 days",
+        tripCode: "MAL404",
         image: "card 7.jpg"
     },
     {
-        name: "Arctic Expedition",
-        destination: "Arctic Circle",
+        name: "Caribbean Cruise Explorer",
+        destination: "Caribbean Islands",
         price: "28000",
+        dateOfDeparture: "2023-12-05",
+        duration: "13 days",
+        tripCode: "CAR505",
+        image: "card 8.jpg"
+    },
+    {
+        name: "Greek Island Odyssey",
+        destination: "Greek Islands",
+        price: "33000",
         dateOfDeparture: "2024-01-15",
         duration: "18 days",
-        tripCode: "ARC606",
-        image: "card 10.jpg"
+        tripCode: "GRE606",
+        image: "card 9.jpg"
     }
 ];
 
 $(document).ready(function(){
-    //select the plants container and add html from our template 
+     
         
     
-    //you have to call it only when ready/loaded
+   
         loadTripOptions();
     });
     
@@ -110,7 +109,7 @@ $(document).ready(function(){
     
             $(currentChild).find("#durationText").text(trips[i].duration);
 
-            $(currentChild).find("#dateOfDepartureTextext").text( trips[i].dateOfDeparture);
+            $(currentChild).find("#dateOfDeparturetext").text( trips[i].dateOfDeparture);
 
             $(currentChild).find("#tripCodeText").text(trips[i].tripCode);
             
@@ -119,11 +118,11 @@ $(document).ready(function(){
     }
     
     
-    $("#plantsContainer").on('click', '.card', function(){
+    $("#tripsContainer").on('click', '.card', function(){
     
-      // Toggle the price & description text
+     
       $(this).find("#priceText").toggle();
-      $(this).find("#descriptionText").toggle();
+      $(this).find("#dateOfDepartureText").toggle();
     
       // Resize the image to fit the additional content
       $(this).find(".card-img-top").toggleClass("small");
@@ -135,22 +134,4 @@ $(document).ready(function(){
 
 
 
-// $(document).ready(function() {
-//     const tripContainer = $("#trip-container");
 
-//     trips.forEach(function(trip) {
-//         const card = `
-//             <div class="card mb-3">
-//                 <div class="card-body">
-//                     <h5 class="card-title">${trip.name}</h5>
-//                     <p class="card-text"><strong>Destination:</strong> ${trip.destination}</p>
-//                     <p class="card-text"><strong>Price:</strong> ${trip.price}</p>
-//                     <p class="card-text"><strong>Date of Departure:</strong> ${trip.dateOfDeparture}</p>
-//                     <p class="card-text"><strong>Duration:</strong> ${trip.duration}</p>
-//                     <p class="card-text"><strong>Trip Code:</strong> ${trip.tripCode}</p>
-//                 </div>
-//             </div>
-//         `;
-//         tripContainer.append(card);
-//     });
-// });
